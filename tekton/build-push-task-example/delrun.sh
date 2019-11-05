a6de21565a2aef9dd144d01812560ddc68fa5638
azkaoru <towadad@gmail.com>
2019-11-05 14:19:54 +0900

    add build-push

---
+++ tekton/build-push-task-example/delrun.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# setting policy
+#
+#for taskrun
+oc delete -f taskrun.yaml 
+
+#for task
+oc delete -f task.yaml
+
+#for pipeline resource
+oc delete -f pipeline-resource.yaml
+
+oc delete -f pipeline-account.yaml
---
+++ tekton/build-push-task-example/readme.md
@@ -0,0 +1,7 @@
+# hello task example
+
+```
+./testrun.sh
+``` 
+
+
